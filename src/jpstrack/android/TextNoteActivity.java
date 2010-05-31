9ed3ea1f45b70714b49ca72db771dc095f7c3462
IanDarwin <>
2010-05-31 16:15:36 +0000

    Rename for consistency.

--- src/jpstrack/android/TextNoteActivity.java
+++ src/jpstrack/android/TextNoteActivity.java
@@ -10,6 +10,7 @@ public class TextNoteActivity extends Activity implements OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.textnote);
 	}
 	
 	@Override
