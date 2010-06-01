01ddd9a0cbb22862db33716f2efba494238da302
IanDarwin <>
2010-06-01 01:36:32 +0000

    Initial stub of a CameraNote activity. Weird name but it goes with VoiceNote and TextNote.

---
+++ src/jpstrack/android/CameraNoteActivity.java
@@ -0,0 +1,42 @@
+package jpstrack.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class CameraNote extends Activity implements SurfaceHolder.Callback, OnClickListener {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
