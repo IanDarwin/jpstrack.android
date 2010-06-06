616da018dfd2677a062116be04e5968a8b52cf01
IanDarwin <>
2010-06-06 18:43:45 +0000

    About now has an OK button, though it doesn't appear in QV resolution.

--- src/jpstrack/android/AboutActivity.java
+++ src/jpstrack/android/AboutActivity.java
@@ -3,12 +3,27 @@ package jpstrack.android;
 import jpstrack.android.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		
+		Button ok = (Button) findViewById(R.id.about_done_button);
+		ok.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.about_done_button:
+			finish();
+			break;
+		}
 	}
 }
