cf8103e22a6dab8af4fcc2e67e88e0ada3db9e63
IanDarwin <>
2010-06-19 20:19:55 +0000

    Condition intent to save the image to our file location. Hints from 'sashomashi', comment 42 at http://code.google.com/p/android/issues/detail?id=1480

--- src/jpstrack/android/CameraNoteActivity.java
+++ src/jpstrack/android/CameraNoteActivity.java
@@ -1,19 +1,34 @@
 package jpstrack.android;
 
+import java.io.File;
+
+import jpstrack.fileio.FileNameUtils;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.widget.Toast;
 
 public class CameraNoteActivity extends Activity {
 
+	private File imageFile;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Use an Intent to get the Camera app going.
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// Set up file to save image into.
+		imageFile = new File(Main.getDataDir(), FileNameUtils.getNextFilename("jpg"));
+		Uri uri = Uri.fromFile(imageFile);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+		intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+		// And away we go!
 		startActivityForResult(intent, 0);
 	}
 	
@@ -23,8 +38,12 @@ public class CameraNoteActivity extends Activity {
 		case 0: // take picture
 			switch(resultCode) {
 			case Activity.RESULT_OK:
-				Bitmap ret = (Bitmap) data.getExtras().get("data");
-				Toast.makeText(this, "Bitmap returned is " + ret.getWidth() + "x" + ret.getHeight(), Toast.LENGTH_LONG).show();
+				if (imageFile.exists())
+					Toast.makeText(this, "Bitmap saved as " + imageFile.getAbsoluteFile(), Toast.LENGTH_LONG).show();
+				else {
+					AlertDialog.Builder alert = new AlertDialog.Builder(this);
+					alert.setTitle("Inconnu").setMessage("Camera Intent returned OK but image not created!").show();
+				}
 				break;
 			case Activity.RESULT_CANCELED:
 				//  no blather required!
