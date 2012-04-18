1d1abd64c0a1ac32236c6ed8c406c8909699b7f0
IanDarwin <>
2012-04-18 01:00:46 +0000

    Use a convoluted URL that works rather than a simple one that doesn't.

--- src/jpstrack/android/ShowWelcomeVideo.java
+++ src/jpstrack/android/ShowWelcomeVideo.java
@@ -1,7 +1,7 @@
 package jpstrack.android;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,13 +15,11 @@ public class ShowWelcomeVideo extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.onboard_video);
 		VideoView vw = (VideoView) findViewById(R.id.onboard_videoView);
-		MediaPlayer player = MediaPlayer.create(this, R.raw.jpstrack_tutorial);
-		if (player != null) {
-			player.start();
-		}
-		MediaController controller = new MediaController(this);
-		controller.setMediaPlayer(vw);
-		vw.setMediaController(controller);
+		// This URL fetches a file from the res/raw folder.
+		String VIDEO_URL = "android.resource://" + getPackageName() +  "/" + R.raw.welcomevideo;
+		vw.setVideoURI(Uri.parse(VIDEO_URL));
+		vw.setMediaController(new MediaController(this));
+		vw.start();
 		
 		Button b = (Button)findViewById(R.id.onboard_videoDoneButton);
 		b.setOnClickListener(new OnClickListener() {
