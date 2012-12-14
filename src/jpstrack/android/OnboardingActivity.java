0a987329fe756c79b6b5bd4d53947ba68e4d7089
IanDarwin <>
2012-12-14 20:01:53 +0000

    Imports police

--- src/jpstrack/android/OnboardingActivity.java
+++ src/jpstrack/android/OnboardingActivity.java
@@ -3,7 +3,6 @@ package jpstrack.android;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.Activity;
