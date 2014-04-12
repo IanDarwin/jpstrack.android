554371b841ace589a4e96c60ae3aa87b72e4d93b
Ian Darwin <ian@darwinsys.com>
2014-04-12 13:12:01 -0400

    Use an Executor

--- src/jpstrack/android/ThreadUtils.java
+++ src/jpstrack/android/ThreadUtils.java
@@ -1,9 +1,14 @@
 package jpstrack.android;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class ThreadUtils {
 	
-	static void execute(final Runnable r) {
-		new Thread(r).start();
+	private final static Executor threadpool = Executors.newFixedThreadPool(1);
+	
+	public static void execute(final Runnable r) {
+		threadpool.execute(r);
 	}
 	
 	static void executeAndWait(final Runnable r) {
