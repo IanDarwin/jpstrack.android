50099527d6bcc7e3e3b63650740d500ba922a22a
Ian Darwin <ian@darwinsys.com>
2014-04-18 12:17:38 -0400

    Enum for use with Google Analytics

---
+++ src/main/java/jpstrack/android/TrackerName.java
@@ -0,0 +1,9 @@
+package jpstrack.android;
+
+/** Tracker, for use with Google Analytics */
+public enum TrackerName {
+	/** The tracker for this application */
+	APP_TRACKER,
+	/** The tracker for all RejmiNet apps */
+	COMPANY_TRACKER,
+}
