dfdb283142dfb170e10b1bf195cc2abf9db7bc72
Ian Darwin <ian@darwinsys.com>
2014-04-18 15:43:00 -0400

    Abolish ever ref. to Google Play for Analytics, in protest over their not having usable Maven artifacts.

--- src/main/java/jpstrack/android/AndroidApplication.java
+++ src/main/java/jpstrack/android/AndroidApplication.java
@@ -5,35 +5,11 @@ import java.util.Map;
 
 import android.app.Application;
 
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Tracker;
-
 /**
  * The Android singleton for this application
  */
 public class AndroidApplication extends Application {
 
-	private static final String PROPERTY_ID_JPSTRACK = "UA-33064318-2";
-	private Map<TrackerName, Tracker> mTrackers = new HashMap<>();
+	// Just a placeholder at the moment.
 
-	synchronized Tracker getTracker(TrackerName trackerId) {
-		if (!mTrackers.containsKey(trackerId)) {
-
-			GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-			Tracker t;
-			switch (trackerId) {
-			case APP_TRACKER:
-				t = analytics.newTracker(PROPERTY_ID_JPSTRACK);
-				break;
-			case COMPANY_TRACKER:
-				t = analytics.newTracker(PROPERTY_ID_JPSTRACK);
-				break;
-			default:
-				throw new IllegalArgumentException("Unhandled enum case: "
-						+ trackerId);
-			}
-			mTrackers.put(trackerId, t);
-		}
-		return mTrackers.get(trackerId);
-	}
 }
