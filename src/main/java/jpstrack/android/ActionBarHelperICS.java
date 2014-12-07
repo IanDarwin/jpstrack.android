65b1a20693f054cd2ddaf014f35cfa757b2f713b
Ian Darwin <ian@darwinsys.com>
2014-12-07 14:22:38 -0500

    Warning police

--- src/main/java/jpstrack/android/ActionBarHelperICS.java
+++ src/main/java/jpstrack/android/ActionBarHelperICS.java
@@ -18,8 +18,6 @@ package jpstrack.android;
 
 import android.app.Activity;
 import android.content.Context;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * An extension of {@link com.example.android.actionbarcompat.ActionBarHelper} that provides Android
